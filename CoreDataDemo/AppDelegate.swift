2d1d3ee0f9de7e1fb1ff031074d9e4c17a7212d0
Maksimova Kristel <idmaksimovak@gmail.com>
2022-03-02 22:59:29 +0300

    Initial Commit

---
+++ CoreDataDemo/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  CoreDataDemo
+//
+//  Created by Kristel Maximova on 06.01.2022.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = UINavigationController(rootViewController: TaskListViewController())
+        
+        return true
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        StorageManager.shared.saveContext()
+    }
+
+}
+
